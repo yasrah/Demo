1dc79a50b23ff70ffc0264d63448ddb85a8ef324
Yaser <m.y.rahmati@gmail.com>
2017-12-13 19:46:32 +0100

    Done: epost validering er lagt til i loginController.

--- Demo.Web/App_Data/Models/models.generated.cs
+++ Demo.Web/App_Data/Models/models.generated.cs
@@ -19,8 +19,8 @@ using Umbraco.ModelsBuilder;
 using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "f78b5a3a2d32617d")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "18ee373e007175de")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
@@ -331,16 +331,16 @@ namespace Umbraco.Web.PublishedContentModels
 		}
 	}
 
-	/// <summary>ShowComplaintReport</summary>
-	[PublishedContentModel("showComplaintReport")]
-	public partial class ShowComplaintReport : PublishedContentModel, IHideInNavDT
+	/// <summary>NewPasswordPage</summary>
+	[PublishedContentModel("newPasswordPage")]
+	public partial class NewPasswordPage : PublishedContentModel
 	{
 #pragma warning disable 0109 // new is redundant
-		public new const string ModelTypeAlias = "showComplaintReport";
+		public new const string ModelTypeAlias = "newPasswordPage";
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 #pragma warning restore 0109
 
-		public ShowComplaintReport(IPublishedContent content)
+		public NewPasswordPage(IPublishedContent content)
 			: base(content)
 		{ }
 
@@ -351,18 +351,9 @@ namespace Umbraco.Web.PublishedContentModels
 		}
 #pragma warning restore 0109
 
-		public static PublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<ShowComplaintReport, TValue>> selector)
+		public static PublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<NewPasswordPage, TValue>> selector)
 		{
 			return PublishedContentModelUtility.GetModelPropertyType(GetModelContentType(), selector);
-		}
-
-		///<summary>
-		/// Skjul i navigasjon
-		///</summary>
-		[ImplementPropertyType("hideInNavigation")]
-		public bool HideInNavigation
-		{
-			get { return Umbraco.Web.PublishedContentModels.HideInNavDT.GetHideInNavigation(this); }
 		}
 	}
 
@@ -401,16 +392,16 @@ namespace Umbraco.Web.PublishedContentModels
 		}
 	}
 
-	/// <summary>NewPasswordPage</summary>
-	[PublishedContentModel("newPasswordPage")]
-	public partial class NewPasswordPage : PublishedContentModel
+	/// <summary>ShowComplaintReport</summary>
+	[PublishedContentModel("showComplaintReport")]
+	public partial class ShowComplaintReport : PublishedContentModel, IHideInNavDT
 	{
 #pragma warning disable 0109 // new is redundant
-		public new const string ModelTypeAlias = "newPasswordPage";
+		public new const string ModelTypeAlias = "showComplaintReport";
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 #pragma warning restore 0109
 
-		public NewPasswordPage(IPublishedContent content)
+		public ShowComplaintReport(IPublishedContent content)
 			: base(content)
 		{ }
 
@@ -421,9 +412,18 @@ namespace Umbraco.Web.PublishedContentModels
 		}
 #pragma warning restore 0109
 
-		public static PublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<NewPasswordPage, TValue>> selector)
+		public static PublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<ShowComplaintReport, TValue>> selector)
 		{
 			return PublishedContentModelUtility.GetModelPropertyType(GetModelContentType(), selector);
+		}
+
+		///<summary>
+		/// Skjul i navigasjon
+		///</summary>
+		[ImplementPropertyType("hideInNavigation")]
+		public bool HideInNavigation
+		{
+			get { return Umbraco.Web.PublishedContentModels.HideInNavDT.GetHideInNavigation(this); }
 		}
 	}
 
--- Demo.Web/Controllers/LoginController.cs
+++ Demo.Web/Controllers/LoginController.cs
@@ -38,30 +38,37 @@ namespace Demo.Web.Controllers
         [HttpPost]
         public ActionResult Email(ForgotenPasswordViewModel model)
         {
-            //TODO: følgende er nødvedig for å validere model og evt vise melding.
-            //    Akkurat nå feiler Send nytt pasord hvis epost ikke utfylt.
-            //if (model sin Email property IKKE er utfylt)
-            //{
-            //    return samme side som bruekren er på og vis en melding met TempData["msg"] osv.
-            //}
-            String username =  Membership.GetUserNameByEmail(model.Email);
+            try
+            {
+                var mail = new System.Net.Mail.MailAddress(model.Email);
+
+                String username = Membership.GetUserNameByEmail(model.Email);
                 MembershipUser user = Membership.GetUser(username);
 
-            if (user != null){
-                string oldpassword=user.ResetPassword();
-                string newPassword= Membership.GeneratePassword(12,2);
-                 
-                bool password = user.ChangePassword(oldpassword,newPassword);
-           
-                umbraco.library.SendMail("m.y.rahmati@gmail.com",model.Email,"newPassword","hei "+ user + " passord endred: "+newPassword+"",false);
-               
+                if (user != null)
+                {
+                    string oldpassword = user.ResetPassword();
+                    string newPassword = Membership.GeneratePassword(12, 2);
+
+                    bool password = user.ChangePassword(oldpassword, newPassword);
+
+                    umbraco.library.SendMail("m.y.rahmati@gmail.com", model.Email, "newPassword", "hei " + user + " passord endred: " + newPassword + "", false);
+
+                }
+                if (user == null)
+                {
+
+                    umbraco.library.SendMail("m.y.rahmati@gmail.com", model.Email, "newPassword", "Ingen bruker med " + model.Email + " er registrert i dette systemet", false);
+
+                }
+                TempData["Status"] = "Sjekk din inbox for nytt passord!";
+
             }
-              if (user==null){
-             
-                umbraco.library.SendMail("m.y.rahmati@gmail.com",model.Email,"newPassword","Ingen bruker med "+ model.Email+" er registrert i dette systemet",false);
-               
+            catch {
+                TempData["Status"] = "Dette er ikke en epost adresse! skriv riktig epost";
+                return RedirectToCurrentUmbracoPage();
+
             }
-            TempData["Status"] = "Sjekk din inbox for nytt passord!";
             return Redirect("/");
             
         }
@@ -74,6 +81,8 @@ namespace Demo.Web.Controllers
             return Redirect("/");
         }
 
-        
+
+       
+
     }
 }
